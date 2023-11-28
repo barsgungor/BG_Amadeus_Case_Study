913ecaed0c354e67efde713aaa25a9247b974cc5
barsgungor <112784203+barsgungor@users.noreply.github.com>
2023-11-28 13:01:13 +0300

    Update Driver.java

--- src/test/java/com/flightsapp/utilities/Driver.java
+++ src/test/java/com/flightsapp/utilities/Driver.java
@@ -18,17 +18,8 @@ public class Driver {
 
     private Driver(){}
 
-    /*
-    We make WebDriver private, because we want to close access from outside the class.
-    We make it static because we will use it in a static method.
-     */
-    //private static WebDriver driver; // value is null by default
-
     private static InheritableThreadLocal<WebDriver> driverPool = new InheritableThreadLocal<>();
 
-    /*
-    Create a re-usable utility method which will return same driver instance when we call it
-     */
     public static WebDriver getDriver(){
 
         if (driverPool.get() == null){
